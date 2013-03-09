b9e8f835617fcb94a33375e7303447fb71ff73e2
Trey Shugart <treshugart@gmail.com>
2013-03-09 13:01:10 +1100

    Go back to using JSON config files fix for API changes.

--- src/Europaphp/Help/Bootstrapper.php
+++ src/Europaphp/Help/Bootstrapper.php
@@ -1,9 +1,23 @@
 <?php
 
 namespace Europaphp\Help;
-use Europa\Module\ModuleBootstrapperAbstract;
+use Europa\Fs\Locator;
+use Europa\Router\Router;
+use Europa\Module\Bootstrapper\BootstrapperAbstract;
 
-class Bootstrapper extends ModuleBootstrapperAbstract
+class Bootstrapper extends BootstrapperAbstract
 {
-    
+    public function routes()
+    {
+        $router = new Router;
+        $router->import($this->module->path() . '/configs/routes.json');
+        $this->injector->get('routers')->append($router);
+    }
+
+    public function views()
+    {
+        $locator = new Locator;
+        $locator->addPath($this->module->path() . '/views');
+        $this->injector->get('viewLocators')->append($locator);
+    }
 }
